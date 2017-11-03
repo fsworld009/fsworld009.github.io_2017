56846d9283bea509c7c1875f8d7e009f5cf6d6be
WorldFS <fsworld009@gmail.com>
2017-11-03 23:19:11 +0800

    local component test

--- source/Index.vue
+++ source/Index.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
+    <about></about>
     <p>{{message}}</p>
   </div>
 </template>
 
 <script>
+import About from "./components/About.vue";
 export default {
   data: function(){
     return {
       message: "Hello World"
     };
-  }
+  },
+  components: {about: About}
 };
 </script>
