5dab2af16b0ec24342ce083a098d43577609f752
WorldFS <fsworld009@gmail.com>
2017-11-04 23:17:41 +0800

    responsive classes and menu

--- source/Index.vue
+++ source/Index.vue
@@ -1,18 +1,31 @@
 <template>
   <div>
-    <about></about>
-    <p>{{message}}</p>
+    <app-menu></app-menu>
+    <div class="ui main text container">
+      <p>{{message}}</p>
+      <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+    </div>
   </div>
 </template>
 
+<style scoped>
+.main.container {
+  margin-top: 7em;
+}
+</style>
+
+
 <script>
 import About from "./components/About.vue";
+import Menu from "./components/Menu.vue";
 export default {
   data: function(){
     return {
       message: "Hello World"
     };
   },
-  components: {about: About}
+  components: {
+    "app-menu": Menu
+  }
 };
 </script>
---
+++ source/components/Menu.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="ui fixed inverted menu blue mobile hidden">
+      <div class="ui right menu inverted blue">
+        <a href="#" class="item">Profile</a>
+        <a href="#" class="item">Projects</a>
+        <a href="#" class="item">Contact</a>
+        <a href="#" class="item">Blog</a>
+      </div>
+    </div>
+    <div class="mobile only mobile-menu">
+      <div class="ui pointing dropdown top right  item">
+        <i class="big home icon"></i>
+        <div class=" menu">
+        <a href="#" class="item">Profile</a>
+        <a href="#" class="item">Projects</a>
+        <a href="#" class="item">Contact</a>
+        <a href="#" class="item">Blog</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.mobile-menu {
+  position: fixed;
+  top: 0;
+  right: 5px;
+}
+
+</style>
+
+
+<script>
+export default {
+  mounted: function() {
+    $(this.$el).find(".mobile-menu").find(".ui.dropdown").dropdown();
+  },
+
+  beforeDestroy: function(){
+    (this.$el).find(".mobile-menu").find(".ui.dropdown").dropdown('destroy');
+  }
+}
+</script>
