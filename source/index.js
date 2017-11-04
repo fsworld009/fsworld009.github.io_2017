58783a44ed741d763173250ff5c6ce3f38eedb21
WorldFS <fsworld009@gmail.com>
2017-11-04 17:15:02 +0800

    Semantic UI, README

--- source/index.js
+++ source/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Index from './Index.vue';
 
+require('./semantic/semantic.css');
+require('./semantic/semantic.js');
+
 new Vue({
   el: "#app",
     template: '<Index/>',
